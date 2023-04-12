7364165b268af0ba3d7ab43e4bc4bee8d68f3264
danyela2000 <terraplanet20@gmail.com>
2023-04-12 11:28:41 +0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,43 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const likeBtn = document.getElementById("like-btn")
+const likeNumber = document.getElementById("likes-number")
+const postImage = document.getElementById("post")
+
+likeBtn.addEventListener("click", function(){
+    posts[0].likes += 1
+    likeNumber.textContent = `${posts[0].likes } likes`
+})
+
+// postImage.addEventListener("dbclick", function(){
+//     posts[0].likes += 1
+//     likeNumber.textContent = `${posts[0].likes } likes`
+// })
